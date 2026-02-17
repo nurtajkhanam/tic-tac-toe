8c4de40e37c4152e595507d33391dad81d6346e7
nurtaj <nurtajkhanam777@gmail.com>
2026-02-17 21:17:24 +0600

    feat:implemented board with clickable square components

---
+++ src/Board.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button
+      className="bg-white border border-gray-400 h-12 m-1 w-12 leading-9 text-lg"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
